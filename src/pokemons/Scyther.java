b9397736f5b206d9bb0a232bee7e1de1c438fdb7
Maria Stetsenko <maria@stetsenko.su>
2023-10-11 14:30:26 +0300

    added all pokemons

---
+++ src/pokemons/Scyther.java
@@ -0,0 +1,12 @@
+package pocemons;
+
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Type;
+
+public class Scyther extends Pokemon {
+    public Scyther(String name, int level) {
+        super(name, level);
+        setStats(70, 110, 80, 55, 80, 105);
+        setType(Type.BUG, Type.FLYING);
+    }
+}
